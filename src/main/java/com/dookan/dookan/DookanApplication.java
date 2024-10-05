b0a0344ccd0002fc8fee030ec99b2602e35e063c
Sachin Hatvalne <sachinhatvalne01@gmail.com>
2024-10-05 21:41:06 +0000

    feat: added springboot project with maven

---
+++ src/main/java/com/dookan/dookan/DookanApplication.java
@@ -0,0 +1,13 @@
+package com.dookan.dookan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DookanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DookanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dookan/dookan/DookanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dookan.dookan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DookanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
